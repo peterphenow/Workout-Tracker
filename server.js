516cad5c1f26b6a1716208b7ecf844bfd650f6f7
Peter Phenow <peter.phenow@gmail.com>
2021-02-05 22:59:26 -0600

    testing adding new documents to collections

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
@@ -20,3 +20,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+// // works to create a new document in the cardioworkouts collection
+// const cardio = new db.Cardio({
+//   name: "Walk",
+//   distance: 1,
+//   duration: 20
+// });
+
+// cardio.save();
+
+// // works to create a new document in the resistanceworkouts collection
+// const resistance = new db.Resistance({
+//   name: "bench press",
+//   weight: 150,
+//   sets: 5,
+//   reps: 10,
+//   duration: 20
+// });
+
+// resistance.save();
