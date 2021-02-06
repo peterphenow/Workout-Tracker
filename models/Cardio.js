d376e97cc93e6345f68fd613224f78c60141b596
Peter Phenow <peter.phenow@gmail.com>
2021-02-05 22:49:32 -0600

    created cardio model

---
+++ models/Cardio.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cardioSchema = new Schema({
+  name: String,
+  distance: Number,
+  duration: Number
+});
+
+// Create Cardio model
+const Cardio = mongoose.model("CardioWorkout", cardioSchema);
