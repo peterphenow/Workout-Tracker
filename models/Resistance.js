582b672a1fa4c47399ee89612491b1178824a78a
Peter Phenow <peter.phenow@gmail.com>
2021-02-05 22:49:53 -0600

    created resistance model

---
+++ models/Resistance.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const resistanceSchema = new Schema({
+  name: String,
+  weight: Number,
+  sets: Number,
+  reps: Number,
+  Duration: Number
+});
+
+// Create Cardio model
+const Resistance = mongoose.model("ResistanceWorkout", resistanceSchema);
