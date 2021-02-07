b0f9e26d2c023f56228e61cd75870e3e9a606a2d
Peter Phenow <peter.phenow@gmail.com>
2021-02-06 19:03:09 -0600

    added route for the stats.html page

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,7 +1,13 @@
 const path = require("path");
 
 module.exports = function (app) {
+  // set up the route linking to exercise.html
   app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  // set up the route linking to stats.html
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 };
