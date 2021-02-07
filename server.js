75138ce4a95b7fa267fef61931557b2b628245c8
Peter Phenow <peter.phenow@gmail.com>
2021-02-06 18:56:14 -0600

    route set up for a new workout

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//require routes
+require("./routes/htmlroutes")(app);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(PORT, () => {
