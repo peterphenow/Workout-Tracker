71e5493ddd11593ea2176f83cafe45c082f5efbb
Peter Phenow <peter.phenow@gmail.com>
2021-02-08 09:29:00 -0600

    find all for range route on stats page is working

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -13,17 +13,25 @@ module.exports = function (app) {
 
   // called when the stats page is opened
   app.get("/api/workouts/range", (req, res) => {
-    console.log("/api/workouts/range???", req.body);
-  });
-
-  //when home page is loaded
-  app.get("/api/workouts", (req, res) => {
-    console.log("-GET /api/workouts??? home page loaded", req.body);
+    // console.log("/api/workouts/range???", req.body);
     db.Workout.find({}, (err, data) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(data);
+        // console.log(data);
+        res.json(data);
+      }
+    });
+  });
+
+  //when home page is loaded
+  app.get("/api/workouts", (req, res) => {
+    // console.log("-GET /api/workouts??? home page loaded", req.body);
+    db.Workout.find({}, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(data);
         res.json(data);
       }
     });
